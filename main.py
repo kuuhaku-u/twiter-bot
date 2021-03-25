5350d579eb2aa30e74f0c9d2a38b19a3a584ccef
kor0 <ryusama6996@gmail.com>
2021-03-25 04:55:37 +0000

    .

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+###########--library---##############
+
 import tweepy
 import os
 
+#######-key_sec--###########
 auth = tweepy.OAuthHandler(os.getenv('consumer_key'),
                            os.getenv('consumer_secret'))
 auth.set_access_token(os.getenv('key'), os.getenv('secret'))
