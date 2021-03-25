5cc2230872554034c1f429e7dd6afb66720672c2
kor0 <ryusama6996@gmail.com>
2021-03-25 04:46:36 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+import tweepy
+import os
+
+auth = tweepy.OAuthHandler(os.getenv('consumer_key'),
+                           os.getenv('consumer_secret'))
+auth.set_access_token(os.getenv('key'), os.getenv('secret'))
+
+
+api = tweepy.API(auth)
+api.update_status('I am Alive')
